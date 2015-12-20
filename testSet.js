79930fc45209127eb1fec80d57b5dfa5a2e584d5
EdwardHuCS <edward.s.hu.cs@gmail.com>
2015-12-19 20:44:36 -0800

    test set of section objects

---
+++ testSet.js
@@ -0,0 +1,51 @@
+[
+  [{
+    type: 'Lec',
+    start_time: '4:30',
+    end_time: '6:50',
+    day: 'M'
+  }, {
+    type: 'Lab',
+    start_time: '14:00',
+    end_time: '15:50',
+    day: 'W'
+  }, {
+    type: 'Qz',
+    start_time: '19:00',
+    end_time: '20:50',
+    day:'F'
+  }],
+
+  [{
+    type: 'Lec',
+    start_time: '4:30',
+    end_time: '6:50',
+    day: 'W'
+  }, {
+    type: 'Lab',
+    start_time: '14:00',
+    end_time: '15:50',
+    day: 'W'
+  }, {
+    type: 'Qz',
+    start_time: '19:00',
+    end_time: '20:50',
+    day:'F'
+  }],
+  [{
+    type: 'Lec',
+    start_time: '4:30',
+    end_time: '6:50',
+    day: 'M'
+  }, {
+    type: 'Lab',
+    start_time: '4:00',
+    end_time: '15:50',
+    day: 'W'
+  }, {
+    type: 'Qz',
+    start_time: '19:00',
+    end_time: '20:50',
+    day:'F'
+  }]
+]
