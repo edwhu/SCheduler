8c483a4c6e76e08da6a214d9ea1fc55edabfcbb5
EdwardHuCS <edward.s.hu.cs@gmail.com>
2015-12-27 16:01:24 -0800

    sends out JSON data

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ var alg = require('./lib/algorithm.js')
 
 //sends out 1st element of schedule array
 app.get('/', function(req, res) {
-  alg.create(function(answer){res.send(answer.toString());});
+  alg.create(function(answer){res.send(answer);});
 });
 
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 });
