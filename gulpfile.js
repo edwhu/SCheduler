26916df556fbc0a0cb197739e4d559f1dbb868cd
EdwardHuCS <edward.s.hu.cs@gmail.com>
2016-01-06 20:19:55 -0800

    added browserify / rectify

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var concat = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
 
 // JS hint task
 gulp.task('jshint', function() {
@@ -15,9 +17,26 @@ gulp.task('jshint', function() {
 
 // JS concat, strip debugging and minify
 gulp.task('scripts', function() {
-  gulp.src(['./lib/algorithm.js','./lib/*.js'])
-    .pipe(concat('script.js'))
+  gulp.src(['./public/scripts/bundle.js'])
+    .pipe(concat('bundle.min.js'))
     .pipe(stripDebug())
     .pipe(uglify())
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('./public/scripts/'));
 });
+
+
+
+gulp.task('browserify', function() {
+  browserify({
+      entries: ['./public/scripts/form.js'],
+      transform: ['reactify'],
+      debug: true
+    })
+    .bundle()
+    //Pass desired output filename to vinyl-source-stream
+    .pipe(source('bundle.js'))
+    // Start piping stream to tasks!
+    .pipe(gulp.dest('./public/scripts'));
+});
+
+gulp.task('build', ['browserify','scripts']);
