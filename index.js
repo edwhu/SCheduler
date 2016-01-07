5da885c8c788440d5ead408a46e733c26be3d713
EdwardHuCS <edward.s.hu.cs@gmail.com>
2016-01-06 20:20:17 -0800

    React Routing

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
-var app = express();
+var path = require('path');
 var alg = require('./lib/algorithm.js');
+var app = express();
 
+app.use('/', express.static(path.join(__dirname, 'public')));
 //sends out 1st element of schedule array
-app.get('/', function(req, res) {
-  alg.create(function(answer){res.send(answer);});
+app.get('/api', function(req, res) {
+  alg.create(function(answer) {
+    res.send(answer);
+  });
 });
 
 var server = app.listen(3000, function() {
