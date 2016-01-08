82ba6d86d71f88e8c2111fad1975319f58e0b565
EdwardHuCS <edward.s.hu.cs@gmail.com>
2016-01-07 18:57:40 -0800

    POST and GET hooks
    now responds to React UI front end

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var alg = require('./lib/algorithm.js');
 var app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 //sends out 1st element of schedule array
 app.get('/api', function(req, res) {
-  alg.create(function(answer) {
+  alg.create('',function(answer) {
     res.send(answer);
   });
 });
 
+app.post('/api', function(req, res){
+  console.log(req.body.course);
+  alg.create(req.body.course, function(answer){res.send(answer);})
+});
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
